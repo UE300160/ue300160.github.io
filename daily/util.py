88cbeeca927ee7292d72bc1ef3b0f4d13094325f
Niko <nikolaos.papadopoulos@univie.ac.at>
2025-11-21 11:58:22 +0100

    FASTA conversion included

--- daily/util.py
+++ daily/util.py
@@ -1,3 +1,22 @@
+codons = {
+    "UUU": "F", "CUU": "L", "AUU": "I", "GUU": "V",
+    "UUC": "F", "CUC": "L", "AUC": "I", "GUC": "V",
+    "UUA": "L", "CUA": "L", "AUA": "I", "GUA": "V",
+    "UUG": "L", "CUG": "L", "AUG": "M", "GUG": "V",
+    "UCU": "S", "CCU": "P", "ACU": "T", "GCU": "A",
+    "UCC": "S", "CCC": "P", "ACC": "T", "GCC": "A",
+    "UCA": "S", "CCA": "P", "ACA": "T", "GCA": "A",
+    "UCG": "S", "CCG": "P", "ACG": "T", "GCG": "A",
+    "UAU": "Y", "CAU": "H", "AAU": "N", "GAU": "D",
+    "UAC": "Y", "CAC": "H", "AAC": "N", "GAC": "D",
+    "UAA": None, "CAA": "Q", "AAA": "K", "GAA": "E",
+    "UAG": None, "CAG": "Q", "AAG": "K", "GAG": "E",
+    "UGU": "C", "CGU": "R", "AGU": "S", "GGU": "G",
+    "UGC": "C", "CGC": "R", "AGC": "S", "GGC": "G",
+    "UGA": None, "CGA": "R", "AGA": "R", "GGA": "G",
+    "UGG": "W", "CGG": "R", "AGG": "R", "GGG": "G"
+}
+
 def read_file_into_lines(file_path):
     """
     A simple function to read a file into
@@ -21,3 +40,4 @@ def read_file_into_lines(file_path):
             cleaned = line.rstrip()
             lines.append(cleaned)
     return lines
+
